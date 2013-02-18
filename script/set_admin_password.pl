c450bd49ebe8648c783d53f8ff5eb7085010fc88
Tim Howe <tim.h@bendtel.com>
2013-02-18 14:38:02 -0800

    fix admin account helper script

--- script/set_admin_password.pl
+++ script/set_admin_password.pl
@@ -27,9 +27,11 @@ my $conn = $cfg->[0]->{'pieng.conf'}->{
                          'Model::PieDB'}->{
                            'connect_info'};
 
-my $schema = PieDB::Schema->connect( $conn->{'dsn'},
-                                     $conn->{'user'},
-                                     $conn->{'password'} );
+my $schema = PieDB::Schema->connect(  $conn->{'dsn'},
+                                      $conn->{'user'},
+                                      $conn->{'password'},
+                                      { 'AutoCommit' => $conn->{'AutoCommit'},
+                                      'quote_names' => $conn->{'quote_names'} } );
 
 my $admin = $schema->resultset('User')->find({
                 username => $admin_user });
@@ -42,6 +44,6 @@ else {
                 username => $admin_user,
                 password => $admin_pass });
     my $role = $schema->resultset('Role')->find(
-                   { name => $admin_user });
+                   { name => 'administrator' });
     $admin->user_roles->create({ role => $role->id });
 }
